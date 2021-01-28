55fabf13efa9fb6f97fd85fc61a9bfa35cd37aa7
smy-101 <shimy1011@foxmail.com>
2021-01-28 22:22:35 +0800

    icon组件添加onClick事件

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Icon from './icon/icon';
 
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target);
+};
+
 ReactDOM.render(<div>
-    <Icon name="page"/>
+    <Icon name="page" onClick={fn}/>
 </div>, document.querySelector('#root'));
